c24631ba70e9a3d69e69508073c952fa3f4fcb07
liubin <412322310@qq.com>
2017-07-22 13:10:34 +0800

    modify for choose condition ui and fragment animation effect

---
+++ src/main/java/com/umasuo/eva/ui/anim/ViewPagerScroller.java
@@ -0,0 +1,62 @@
+package com.umasuo.eva.ui.anim;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.view.animation.Interpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
+
+/**
+ * Fragment之间切换动画效果
+ */
+
+public class ViewPagerScroller extends Scroller{
+
+
+
+    private int mScrollDuration = 0; // 滑动速度
+
+    public ViewPagerScroller(Context context) {
+        super(context);
+    }
+    /**
+     * 设置速度速度
+     *
+     * @param duration
+     */
+    public void setScrollDuration(int duration) {
+        this.mScrollDuration = duration;
+    }
+
+    public ViewPagerScroller(Context context, Interpolator interpolator) {
+        super(context, interpolator);
+    }
+
+    public ViewPagerScroller(Context context, Interpolator interpolator,
+                             boolean flywheel) {
+        super(context, interpolator, flywheel);
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+        super.startScroll(startX, startY, dx, dy, mScrollDuration);
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy) {
+        super.startScroll(startX, startY, dx, dy, mScrollDuration);
+    }
+
+    public void initViewPagerScroll(ViewPager viewPager) {
+        try {
+            Field mScroller = ViewPager.class.getDeclaredField("mScroller");
+            mScroller.setAccessible(true);
+            mScroller.set(viewPager, this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
