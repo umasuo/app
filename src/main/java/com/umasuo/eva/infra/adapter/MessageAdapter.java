b23c88011f92327edf015b7793655024d6af53fb
umasuo <liuquan89@gmail.com>
2017-07-24 21:31:24 +0800

    refactor message center

--- src/main/java/com/umasuo/eva/infra/adapter/MessageAdapter.java
+++ src/main/java/com/umasuo/eva/infra/adapter/MessageAdapter.java
@@ -5,10 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.umasuo.eva.MainActivity;
 import com.umasuo.eva.R;
 import com.umasuo.eva.infra.log.LogControl;
 
@@ -20,15 +18,15 @@ import java.util.Map;
  * 个人中心界面适配器
  */
 
-public class PersonalAdapter extends BaseAdapter {
+public class MessageAdapter extends BaseAdapter {
 
-    private String TAG = "PersonalAdapter";
+    private String TAG = "MessageAdapter";
 
     private LayoutInflater inflater;
-    private List<Map<String, Object>> data;
+    private List<Map<String, String>> data;
     private Context context;
 
-    public PersonalAdapter(Context context, List<Map<String, Object>> data) {
+    public MessageAdapter(Context context, List<Map<String, String>> data) {
         this.inflater = LayoutInflater.from(context);
         this.data = data;
         this.context = context;
@@ -51,41 +49,39 @@ public class PersonalAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        PersonalAdapter.ViewHolder holder;
+        MessageAdapter.ViewHolder holder;
         if (view == null) {
-            holder = new PersonalAdapter.ViewHolder();
-            view = inflater.inflate(R.layout.personal_item, null);
-            holder.img_left = (ImageView) view.findViewById(R.id.personal_left_icon);
-            holder.name = (TextView) view.findViewById(R.id.personal_name);
-            holder.img_right = (ImageView) view.findViewById(R.id.personal_right_arrow);
+            holder = new MessageAdapter.ViewHolder();
+            view = inflater.inflate(R.layout.personal_message_item, null);
+            holder.title = (TextView) view.findViewById(R.id.msg_title);
+            holder.content = (TextView) view.findViewById(R.id.msg_content);
             holder.position = i;
             view.setTag(holder);
         } else {
-            holder = (PersonalAdapter.ViewHolder) view.getTag();
+            holder = (MessageAdapter.ViewHolder) view.getTag();
         }
-        holder.img_left.setBackgroundResource((Integer) data.get(i).get("icon"));
-        holder.name.setText((String) data.get(i).get("name"));
+        holder.title.setText(data.get(i).get("title"));
+        holder.content.setText(data.get(i).get("content"));
         return view;
     }
 
     public static class ViewHolder {
-        public ImageView img_left;
-        public TextView name;
-        public ImageView img_right;
+        public TextView title;
+        public TextView content;
         public int position;
     }
 
     public class MyOnClickListener implements View.OnClickListener {
-        private PersonalAdapter.ViewHolder mviewHolder;
+        private MessageAdapter.ViewHolder mviewHolder;
 
-        public MyOnClickListener(PersonalAdapter.ViewHolder viewHolder) {
+        public MyOnClickListener(MessageAdapter.ViewHolder viewHolder) {
             mviewHolder = viewHolder;
         }
 
         @Override
         public void onClick(View view) {
             LogControl.debug(TAG, "position = " + mviewHolder.position);
-            ((MainActivity) context).showPage(3);
+//            ((MainActivity) context).showPage(3);
         }
     }
 }
--- src/main/java/com/umasuo/eva/ui/personal/MessageCenter.java
+++ src/main/java/com/umasuo/eva/ui/personal/MessageCenter.java
@@ -1,24 +1,70 @@
 package com.umasuo.eva.ui.personal;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.umasuo.eva.MainActivity;
 import com.umasuo.eva.R;
 import com.umasuo.eva.infra.FragmentRoot;
+import com.umasuo.eva.infra.adapter.MessageAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by umasuo on 17/7/7.
  * 个人消息中心
  */
-public class MessageCenter extends FragmentRoot {
+public class MessageCenter extends FragmentRoot implements View.OnClickListener {
+    private static final String TAG = "MessageCenter";
+
+    private MainActivity mainActivity;
+
+    private ImageView backImage;
+
+    private ListView messageList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.message, container, false);
+        View view = inflater.inflate(R.layout.personal_message, container, false);
+        mainActivity = (MainActivity) getContext();
+
+        backImage = (ImageView) view.findViewById(R.id.back);
+        backImage.setOnClickListener(this);
+
+        messageList = (ListView) view.findViewById(R.id.message_list);
+        messageList.setAdapter(new MessageAdapter(mainActivity, getData()));
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back: {
+                mainActivity.popBackStack();
+                break;
+            }
+        }
+    }
+
+    public List<Map<String, String>> getData() {
+        List<Map<String, String>> data = new ArrayList<>();
+
+        Map<String, String> msg1 = new HashMap<>();
+        msg1.put("title", "新版发布");
+        msg1.put("content", "新版更新了重大功能！提供了室内VR效果体验...");
+
+        data.add(msg1);
+
+        return data;
     }
 }
